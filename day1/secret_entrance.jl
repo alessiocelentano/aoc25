edd8090ad6ba62d6ed1bfcb044e25b06eee41df6
Alessio Celentano <alessiocelentano2003@gmail.com>
2025-12-03 20:56:54 +0100

    style: edit spacing

--- day1/secret_entrance.jl
+++ day1/secret_entrance.jl
@@ -2,6 +2,7 @@ const INPUT_FILE_NAME = "input.txt"
 const DIAL_STARTING_POSITION = 50
 const MAX_DIAL_NUMBER = 100
 
+
 function get_op(char::Char)::Function
     if char == 'L'
         return (-)
@@ -9,6 +10,7 @@ function get_op(char::Char)::Function
         return (+)
     end
 end
+
 
 function part2!(dial_position::Ref{Int}, rotations::Int)::Int
     already_counted_zero = dial_position[] == 0
@@ -29,6 +31,7 @@ function part2!(dial_position::Ref{Int}, rotations::Int)::Int
     return out
 end
 
+
 function part1!(dial_position::Ref{Int}, rotations::Int)::Int
     dial_position[] = (dial_position[] + rotations) % MAX_DIAL_NUMBER
     if dial_position[] < 0
@@ -36,6 +39,7 @@ function part1!(dial_position::Ref{Int}, rotations::Int)::Int
     end
     return dial_position[] == 0
 end
+
 
 function secret_entrance(fn!::Function, input_file_name::String)::Int
     lines = readlines(input_file_name)
@@ -50,11 +54,13 @@ function secret_entrance(fn!::Function, input_file_name::String)::Int
 
     return out
 end
+
     
 function main()
     println(secret_entrance(part1!, INPUT_FILE_NAME))
     println(secret_entrance(part2!, INPUT_FILE_NAME))
 end
+
 
 if abspath(PROGRAM_FILE) == @__FILE__
     main()
